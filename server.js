a5db18d055e73084f11e1601ce162199712f902d
yotvat <idooy11@gmail.com>
2024-03-27 15:32:51 +0200

    Backend basic funcionality

---
+++ server.js
@@ -0,0 +1,111 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { toyService } from "./services/toy.service.js";
+
+const app = express()
+
+// Express App Config
+app.use(express.json())
+app.use(cookieParser())
+app.use(express.static('public'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('public'));
+} else {
+    const corsOptions = {
+        origin: [
+            'http://127.0.0.1:3000',
+            'http://localhost:3000',
+            'http://localhost:5173',
+            'http://127.0.0.1:5173',
+        ],
+        credentials: true
+    };
+    app.use(cors(corsOptions));
+}
+
+app.get('/api/toy', (req, res) => {
+    console.log(req.query)
+
+    const { filterBy, sortBy } = req.query
+    
+
+    // const filterBy = {
+    //     txt: filterBy.txt || '',
+    //     maxPrice: filterBy.maxPrice || '',
+    //     inStock: filterBy.inStock || 'all'
+    // }
+    // const sortBy = {
+    //     name: sortBy.name || 1,
+    //     created:sortBy.created
+    // }
+
+
+    // const { filterBy = {}, sortBy = {} } = req.query.params
+
+    // const { filterBy } = req.query.params || {}
+    // const { sortBy } = req.query.params || {}
+
+    toyService.query(filterBy, sortBy)
+        .then(toys => {
+            res.send(toys)
+        })
+        .catch(err => {
+            console.log('Had issues getting toys', err);
+            res.status(400).send({ msg: 'Had issues getting toys' })
+        })
+})
+
+app.get('/api/toy/:id', (req, res) => {
+    const toyId = req.params.id
+    toyService.getById(toyId)
+        .then(toy => {
+            res.send(toy)
+        })
+        .catch(err => {
+            console.log('Had issues getting toy', err);
+            res.status(400).send({ msg: 'Had issues getting toy' })
+        })
+})
+
+app.delete('/api/toy/:id', (req, res) => {
+    const toyId = req.params.id
+    toyService.remove(toyId)
+        .then(() => {
+            res.end('Done!')
+        })
+        .catch(err => {
+            console.log('Had issues deleting toy', err);
+            res.status(400).send({ msg: 'Had issues deleteing toy' })
+        })
+})
+
+app.post('/api/toy', (req, res) => {
+    const toy = req.body
+    toyService.save(toy)
+        .then(savedToy => {
+            res.send(savedToy)
+        })
+        .catch(err => {
+            console.log('Had issues adding toy', err);
+            res.status(400).send({ msg: 'Had issues adding toy' })
+        })
+})
+
+app.put('/api/toy/:id', (req, res) => {
+    const toy = req.body
+    toyService.save(toy)
+        .then(savedToy => {
+            res.send(savedToy)
+        })
+        .catch(err => {
+            console.log('Had issues updating toy', err);
+            res.status(400).send({ msg: 'Had issues updating toy' })
+        })
+})
+
+const port = 3030
+app.listen(port, () => {
+    console.log('Server is up and listening to', port);
+})
---
+++ services/toy.service.js
@@ -0,0 +1,76 @@
+
+import fs from 'fs'
+import { utilService } from './util.service.js'
+const toys = utilService.readJsonFile('data/toy.json')
+
+export const toyService = {
+    query,
+    getById,
+    save,
+    remove
+}
+
+function query(filterBy, sortBy) {
+
+    console.log('filterBy', filterBy)
+    // no need - 
+    if (!filterBy && !sort) return Promise.resolve(toys)
+
+    let toysToReturn = toys
+    if (filterBy.name) {
+
+        const regExp = new RegExp(filterBy.name, 'i')
+        toysToReturn = toysToReturn.filter(toy => regExp.test(toy.name))
+    }
+
+    if (filterBy.maxPrice) {
+        toysToReturn = toysToReturn.filter(toy => toy.price >= filterBy.maxPrice)
+    }
+    if (filterBy.inStock !== 'all') {
+        toysToReturn = toysToReturn.filter(toy => toy.inStock.toString() === filterBy.inStock)
+    }
+
+    //sort
+    if (sortBy.name) {
+        toysToReturn = toysToReturn.sort((t1, t2) => (t1.name.localeCompare(t2.name)) * sortBy.name)
+    }
+
+    if (sortBy.price) {
+        toysToReturn = toysToReturn.sort((t1, t2) => (t1.price - t2.price) * sortBy.price)
+    }
+    if (sortBy.created) {
+        toysToReturn = toysToReturn.sort((t1, t2) => (t1.createdAt - t2.createdAt) * sortBy.created)
+    }
+
+    return Promise.resolve(toysToReturn);
+}
+
+function getById(_id) {
+    const toy = toys.find(toy => toy._id === _id)
+    //
+    return Promise.resolve(toy);
+}
+
+function remove(_id) {
+    const idx = toys.findIndex(toy => toy._id === _id)
+    toys.splice(idx, 1);
+    _saveToysToFile()
+    return Promise.resolve();
+}
+
+function save(toy) {
+    if (toy._id) {
+        const idx = toys.findIndex(currToy => currToy._id === toy._id)
+        toys[idx] = { ...toys[idx], ...toy }
+    } else {
+        toy.createdAt = Date.now();
+        toy._id = utilService.makeId();
+        toys.unshift(toy);
+    }
+    _saveToysToFile();
+    return Promise.resolve(toy);
+}
+
+function _saveToysToFile() {
+    fs.writeFileSync('data/toy.json', JSON.stringify(toys, null, 2));
+}
