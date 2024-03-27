a19d074b756ca7d804eb2fe4949de300ce7fc9e0
yotvat <idooy11@gmail.com>
2024-03-27 23:35:27 +0200

    Finish basic design

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ if (process.env.NODE_ENV === 'production') {
             'http://localhost:3000',
             'http://localhost:5173',
             'http://127.0.0.1:5173',
+            'http://localhost:5175',
         ],
         credentials: true
     };
